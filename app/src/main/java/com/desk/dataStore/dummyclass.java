279fcded3cfd7becc8361d392a545bc8ef7ca9b3
kshitij.sharma <kshitij.sharma@lge.com>
2015-09-30 16:51:50 +0530

    basic structure updated 3

---
+++ app/src/main/java/com/desk/dataStore/dummyclass.java
@@ -0,0 +1,7 @@
+package com.desk.dataStore;
+
+/**
+ * Created by kshitij.sharma on 9/30/2015.
+ */
+public class dummyclass {
+}
---
+++ app/src/main/java/com/desk/networkController/dummyclass.java
@@ -0,0 +1,7 @@
+package com.desk.networkController;
+
+/**
+ * Created by kshitij.sharma on 9/30/2015.
+ */
+public class dummyclass {
+}
