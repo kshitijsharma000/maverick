7fd6d6b2ad44d8200186979165d2490146155bd3
kshitij.sharma <kshitij.sharma@lge.com>
2015-10-07 11:25:38 +0530

    added fragment home again

---
+++ app/src/main/java/com/desk/MainUI/Fragment_home.java
@@ -0,0 +1,50 @@
+package com.desk.mainUI;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.desk.R;
+
+/**
+ * Created by kshitij.sharma on 10/7/2015.
+ */
+public class Fragment_home extends Fragment {
+    private static final String ARG_SECTION_NUMBER = "section_number";
+    private Context context;
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static Fragment_credits newInstance(int sectionNumber) {
+        Fragment_credits fragment = new Fragment_credits();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public Fragment_home() {
+        System.out.println("created fragment home");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_credits, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(
+                getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+
+}
