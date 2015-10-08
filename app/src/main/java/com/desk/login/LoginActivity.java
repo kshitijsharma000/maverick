56df9734173c91633a732ec5f71340f84b67ce04
RohitAntil <rohit.ntil@gmail.com>
2015-10-08 12:26:13 +0530

    Adding login screen

--- app/src/main/java/com/desk/login/LoginActivity.java
+++ app/src/main/java/com/desk/login/LoginActivity.java
@@ -8,36 +8,65 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.desk.R;
+import com.desk.dataStore.LoginDataBaseAdapter;
+
 import android.view.View.OnClickListener;
-public class LoginActivity extends Activity implements OnClickListener {
-TextView register ;
+import android.widget.Toast;
+
+public class LoginActivity extends Activity  {
+    TextView register ;
     Button signin;
+    EditText email , password;
+    LoginDataBaseAdapter loginDataBaseAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // Get The Refference Of Buttons
         register=(TextView) findViewById(R.id.tSignup);
         signin=(Button) findViewById(R.id.btnSingIn);
-        register.setOnClickListener(this);
-        signin.setOnClickListener(this);
+
+
+        email=(EditText)findViewById(R.id.etsEmail);
+        password=(EditText)findViewById(R.id.etsPassw);
+
+        loginDataBaseAdapter=new LoginDataBaseAdapter(this);
+        loginDataBaseAdapter=loginDataBaseAdapter.open();
+
+
+
+
+        // Set OnClick Listener on SignUp button
+        register.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                /// Create Intent for SignUpActivity  abd Start The Activity
+                Intent intentSignUP = new Intent(getApplicationContext(), signUp.class);
+                startActivity(intentSignUP);
+            }
+        });
+        signin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                String myemail=email.getText().toString();
+                String mypassword=password.getText().toString();
+                /// Create Intent for SignUpActivity  abd Start The Activity
+                String storedPassword=loginDataBaseAdapter.getSinlgeEntry(myemail);
+                if(mypassword.equals(storedPassword))
+                {
+                    Toast.makeText(LoginActivity.this, "Congrats: Login Successfull", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
     }
-    public void onClick(View v) {
-        switch(v.getId()) {
-            case R.id.tSignup :
-                Intent reg = new Intent(this,signUp.class);
-                startActivity(reg);
-            case R.id.btnSingIn:
 
-
-
-        }
-
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
